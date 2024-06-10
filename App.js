2ba09b294698fdf95074c639f7d7065ded9a6e66
Aishwarya Iyer <74247717+Aishwarya2701@users.noreply.github.com>
2024-06-10 14:01:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import StopWatch from './components/StopWatch';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StopWatch/>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
